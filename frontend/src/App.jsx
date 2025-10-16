1ed8270efafb51f3fbae986c569bcc22d50c54bc
Sedera_Henintsoa <SederaHenintsoa94@gmail.com>
2025-10-16 12:23:55 +0300

    Final cleanup and commit of project structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center w-[100vw] h-[100vh] bg-black">
+      <div className="text-amber-50 text-4xl ">Welcome to this project!</div>
+    </div>
+  );
+}
+
+export default App;
